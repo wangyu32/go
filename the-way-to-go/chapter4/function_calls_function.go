6c67166a4ebcd1b585ee81c413c5dc03cfc37998
wangyu32 <285677770@qq.com>
2018-09-19 21:27:30 +0800

    4.6 4.7

--- the-way-to-go/chapter4/function_calls_function.go
+++ the-way-to-go/chapter4/function_calls_function.go
@@ -1,19 +1,15 @@
 package main
 
-var a string
+import "fmt"
+import "unicode/utf8"
 
 func main() {
-	a = "G"
-	print(a)
-	f1()
-}
 
-func f1() {
-	a := "O"
-	print(a)
-	f2()
-}
+	str1 := "asSASA ddd dsjkdsjs dk"
+	fmt.Printf("The number of bytes in string str1 is %d\n", len(str1))
+	fmt.Printf("The number of characters in string str1 is %d\n", utf8.RuneCountInString(str1))
+	str2 := "asSASA ddd dsjkdsjsこん dk"
+	fmt.Printf("The number of bytes in string str2 is %d\n", len(str2))
+	fmt.Printf("The number of characters in string str2 is %d", utf8.RuneCountInString(str2))
 
-func f2() {
-	print(a)
 }
