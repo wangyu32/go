574251b3ead365ff2fcf8d7ed181c6602d140d20
wangyu32 <285677770@qq.com>
2018-09-24 02:18:14 +0800

    go 数组使用

---
+++ the-way-to-go/leetcode/intersect.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("vim-go")
+
+	printArray(intersect(nil, nil))
+	printArray(intersect([]int{1}, nil))
+	printArray(intersect([]int{3}, nil))
+	printArray(intersect([]int{1, 2, 3}, nil))
+	printArray(intersect(nil, []int{4}))
+	printArray(intersect(nil, []int{2, 3, 4}))
+
+}
+
+func intersect(nums1 []int, nums2 []int) []int {
+	if len(nums1) == 1 {
+		return nums1
+	}
+	if len(nums2) == 1 {
+		return nums2
+	}
+
+	return nil
+}
+
+func printArray(array []int) {
+	if array == nil {
+		fmt.Println(nil)
+		return
+	}
+
+	for i := 0; i < len(array); i++ {
+		fmt.Printf("%d, ", array[i])
+	}
+
+	fmt.Println()
+
+}
